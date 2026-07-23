23b384a9cf00bd590cd3711c97293a561591bcff
Dinesh121 <111.dinesh@gmail.com>
2026-07-23 18:42:49 -0400

    chore(terraform): add locals.tf (6/31)

---
+++ locals.tf
@@ -0,0 +1,61 @@
+locals {
+  common_tags_r0 = {
+    application         = var.r0_application
+    environment         = var.r0_environment
+    owner               = var.r0_owner
+    cost_center         = var.r0_cost_center
+    business_unit       = var.r0_business_unit
+    data_classification = var.r0_data_classification
+    criticality         = var.r0_criticality
+  }
+
+  common_tags_r1 = {
+    application         = var.r1_application
+    environment         = var.r1_environment
+    owner               = var.r1_owner
+    cost_center         = var.r1_cost_center
+    business_unit       = var.r1_business_unit
+    data_classification = var.r1_data_classification
+    criticality         = var.r1_criticality
+  }
+
+  common_tags_r2 = {
+    application         = var.r2_application
+    environment         = var.r2_environment
+    owner               = var.r2_owner
+    cost_center         = var.r2_cost_center
+    business_unit       = var.r2_business_unit
+    data_classification = var.r2_data_classification
+    criticality         = var.r2_criticality
+  }
+
+  common_tags_r3 = {
+    application         = var.r3_application
+    environment         = var.r3_environment
+    owner               = var.r3_owner
+    cost_center         = var.r3_cost_center
+    business_unit       = var.r3_business_unit
+    data_classification = var.r3_data_classification
+    criticality         = var.r3_criticality
+  }
+
+  common_tags_r4 = {
+    application         = var.r4_application
+    environment         = var.r4_environment
+    owner               = var.r4_owner
+    cost_center         = var.r4_cost_center
+    business_unit       = var.r4_business_unit
+    data_classification = var.r4_data_classification
+    criticality         = var.r4_criticality
+  }
+
+  common_tags_r5 = {
+    application         = var.r5_application
+    environment         = var.r5_environment
+    owner               = var.r5_owner
+    cost_center         = var.r5_cost_center
+    business_unit       = var.r5_business_unit
+    data_classification = var.r5_data_classification
+    criticality         = var.r5_criticality
+  }
+}
